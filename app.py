4de361ccefd714d5a2b0bc4c3dad6ed2906c0acb
devron04 <rounakjha0412@gmail.com>
2026-05-09 20:16:13 +0530

    add app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# Ensure the root directory is in the path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from src.app import demo
+
+if __name__ == "__main__":
+    demo.launch(server_name="0.0.0.0", server_port=7860)
